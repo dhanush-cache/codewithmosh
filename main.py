1f52c8fa09b5089b8c61d71815d949e29afb648b
Anonymous <anonymous@example.com>
2025-01-19 17:11:44 +0530

    Use argparse for command-line arguments.

--- main.py
+++ main.py
@@ -1,34 +1,63 @@
 import json
 import os
 from pathlib import Path
-import sys
+import argparse
 
 from course import CourseSerializer
 from utils import extract_non_videos, extract_videos
+
+
+def list_configs(courses):
+    print("Available configurations:")
+    for i, course in enumerate(sorted(courses.keys()), 1):
+        print(f"{i:02}. {course}")
 
 
 def main():
     config_file = Path("configs.json")
     courses = json.loads(config_file.read_text())
 
-    home = Path("/sdcard") if "ANDROID_STORAGE" in os.environ else Path.home()
+    parser = argparse.ArgumentParser(
+        prog="Code With Mosh",
+        description="Organizes courses from codewithmosh.com",
+        epilog="Checkout https://codewithmosh.com",
+    )
 
-    if len(sys.argv) != 2:
-        print("Usage: python configs.py <config>")
-        print("Available configs:")
-        for course in sorted(courses.keys()):
-            print(f"- {course}")
-        sys.exit(1)
+    parser.add_argument("config", type=str, nargs="?", help="The configuration to use")
+    parser.add_argument(
+        "-l",
+        "--list-configs",
+        action="store_true",
+        help="List all available configurations",
+    )
+    parser.add_argument(
+        "-i",
+        "--input-archive",
+        help="Path to the input file",
+    )
 
-    data = courses[sys.argv[1]]
-    source_dir = next(home.glob("Download*"))
-    data["source"] = source_dir / data["source"]
-    data["target"] = home / "Programming Videos"
+    args = parser.parse_args()
+
+    if args.list_configs:
+        list_configs(courses)
+        parser.exit()
+
+    if not args.config:
+        parser.error("The following arguments are required: config")
+
+    HOME = Path("/sdcard") if "ANDROID_STORAGE" in os.environ else Path.home()
+    downloads = next(HOME.glob("Download*"))
+
+    data = courses[args.config]
 
     slug = data["slug"]
-    source = data["source"]
+    source = (
+        Path(args.input_archive)
+        if args.input_archive
+        else (downloads / f"{args.config}.zip")
+    )
     course = CourseSerializer.get_course(slug)
-    target = data["target"]
+    target = HOME / "Programming Videos"
     intro = data["intro"]
     others = data["others"]
     target_list = course.get_videos(target)
