50a44b2204f00d40640995392fe72f8cab071f05
Anonymous <anonymous@example.com>
2025-01-19 20:50:23 +0530

    Extract templates to remove dulication

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from course import CourseSerializer
 from utils import extract_non_videos, extract_videos
 
 
+HOME = Path("/sdcard") if "ANDROID_STORAGE" in os.environ else Path.home()
+
+
 def list_configs(courses):
     print("Available configurations:")
     for i, course in enumerate(sorted(courses.keys()), 1):
@@ -14,15 +17,11 @@ def list_configs(courses):
 
 
 def main():
-    config_file = Path("configs.json")
-    courses = json.loads(config_file.read_text())
-
     parser = argparse.ArgumentParser(
         prog="Code With Mosh",
         description="Organizes courses from codewithmosh.com",
         epilog="Checkout https://codewithmosh.com",
     )
-
     parser.add_argument("config", type=str, nargs="?", help="The configuration to use")
     parser.add_argument(
         "-l",
@@ -30,13 +29,12 @@ def main():
         action="store_true",
         help="List all available configurations",
     )
-    parser.add_argument(
-        "-i",
-        "--input-archive",
-        help="Path to the input file",
-    )
-
+    parser.add_argument("-i", "--input-archive", help="Path to the input file")
     args = parser.parse_args()
+
+    config_file = Path("data.json")
+    data = json.loads(config_file.read_text())
+    courses = data["configs"]
 
     if args.list_configs:
         list_configs(courses)
@@ -45,12 +43,12 @@ def main():
     if not args.config:
         parser.error("The following arguments are required: config")
 
-    HOME = Path("/sdcard") if "ANDROID_STORAGE" in os.environ else Path.home()
     downloads = next(HOME.glob("Download*"))
 
-    data = courses[args.config]
+    course_data = courses[args.config]
 
-    slug = data["slug"]
+    slug, template_id = course_data.values()
+    intro, others = data["templates"][template_id]
     source = (
         Path(args.input_archive)
         if args.input_archive
@@ -58,8 +56,6 @@ def main():
     )
     course = CourseSerializer.get_course(slug)
     target = HOME / "Programming Videos"
-    intro = data["intro"]
-    others = data["others"]
     target_list = course.get_videos(target)
     extract_videos(source, target_list, ffmpeg=True, intro=intro, others=others)
     extract_non_videos(source, target / str(course))
